4739f80478222eba020853570ce7e5868817e285
Sjones352 <sjones@tistatech.com>
2018-03-10 21:44:52 -0500

    added an invoice model and changed database to postgres

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :invoices
+  resources :logins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'invoices#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
